b5ff9bc45ec6e93d65d5250e4a8dc0353a06cf48
Fredrich Ombico <fombico@pivotal.io>
2017-09-13 23:09:20 -0400

    Removed unsued dependencies

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var app = require('express')(),
 	morgan = require('morgan'),
-	bodyParser = require('body-parser'),
 	port = process.env.PORT || 8080,
-	basicAuth = require('express-basic-auth'),
 	session = require('express-session'),
 	cookieParser = require('cookie-parser'),
 	useragent = require('express-useragent');
-
-app.set('view engine', 'ejs'); // view engine to EJS (default is jade)
 
 app.use(cookieParser());
 app.use(session({
@@ -18,7 +14,6 @@ app.use(session({
 
 app.use(useragent.express());
 app.use(morgan('combined')); // logging of requests
-app.use(bodyParser.urlencoded({extended: false})); // converts request body to json
 
 app.get('*', (req, res) => {
 	if (!req.session.useragent) {
